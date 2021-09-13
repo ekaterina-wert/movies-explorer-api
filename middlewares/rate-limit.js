896ea04a5f740fd89f2ac9a5b243cd8769af41bb
unknown <euveret@yandex.ru>
2021-09-13 14:37:55 +0500

    bugfix: add methods

--- middlewares/rate-limit.js
+++ middlewares/rate-limit.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 50, // limit each IP to 50 requests per windowMs
+  max: 100, // limit each IP to 100 requests per windowMs
 });
 
 module.exports = limiter;
